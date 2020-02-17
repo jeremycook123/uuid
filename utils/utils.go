a7781af09473cc3ef339f34f56ed8253c2b107ef
Jeremy Cook <jeremycook123@gmail.com>
2020-02-18 11:55:01 +1300

    updated api - added GetDoubleUUID()

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,14 @@
 package utils
 
 import "github.com/google/uuid"
+import "fmt"
 
 //GetUUID returns a new UUID
 func GetUUID() uuid.UUID {
 	return uuid.New()
+}
+
+//GetDoubleUUID returns a new double length UUID
+func GetDoubleUUID() string {
+	return fmt.Sprintf("%s-%s\n", uuid.New().String(), uuid.New().String())
 }
